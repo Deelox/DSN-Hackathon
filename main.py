1e5dcd5244497d5bc1628951cec87c2015756081
Deelox <96887411+Deelox@users.noreply.github.com>
2022-07-11 08:00:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+count_of_Words = len('Daniel is a good guy'.split())
+print('Count of Words in the in the given sentence:',count_of_Words)
+print(len('Life is good'.split()))
+outcome = str(input('Enter a sentence:'))
+print(len(outcome))
+
